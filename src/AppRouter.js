7fe8e304a16896b046e6a6a0d3919c763f13df02
Sharon Kennedy <sharon@nomadiclabs.ca>
2018-10-13 18:04:09 -0400

    fetch bikes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -13,6 +13,7 @@ const Entry = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/bikes/" component={BikeIndex} />
       <Route path="/bikes/new" component={NewBike} />
       <Route path="/loan" component={LoanWizard} />
       <Route path="/users/new" component={NewUser} />
