e2bd434e1e8410452a288ef9aa114d145be0b5af
ericauv <ericauv17@gmail.com>
2018-10-14 10:59:29 -0400

    Populates BikeLabel of Bikes that are loaned by user on individualBorrowerPage

--- src/Components/users/Show.js
+++ src/Components/users/Show.js
@@ -22,74 +22,56 @@ export default class ShowUser extends React.Component {
           Role: null, 
           history:null,
           photos:null,
-          PhotoConsent: null, //boolean -- did they give consent for photos to be published
+          PhotoConsent: null //boolean -- did they give consent for photos to be published
         }
       }, 
       bikes: {
         data: {
-          data: null
+          data: [{
+              BikeLabel: null,
+              Class: null,
+              overDue: null,
+              dueDate: null}]
         }
-      },
-      bikesLoaned: [
-                    {
-                      ID_Bike: null, 
-                      overDue: null, 
-                      dueDate: null
-                    }
-                    ]
       }
-    };
-  
-
-  // class bike {
-  //   constructor(id, overDue, dueDate) {
-  //     this.id = id;
-  //     this.overDue = overDue;
-  //     this.dueDate = dueDate;
-  //   }
-  // }
-
-  componentDidMount() {
-    const id = this.props.match.params.id
-    // fetch bike record from api using id
-    console.log('id', id)
-    
-    //get bikes that are On Loan to the User with id
-    //ID_Status = 3 = On Loan
-    API.get(`bikes`, {'filters[ID_User]': id, 'filters[ID_Status]':3 }).then(data => {
-      console.log(data)
-      this.setState({ bikes: data})
-    });
-    API.get(`users/${id}`).then(userData => {
-      console.log(userData)
-      this.setState({user: userData})
-    });
-    let bikesArray = this.state.bikes.data.data;
-    if(bikesArray.length > 0){
-      bikesArray.map(function(element) {
-        bikesLoaned.push({ID_Bike: element.ID_Bike,
-                         overDue: null, 
-                         dueDate: null});
-      }
-    }    
-  }
-  getPhoto(){
-    if(this.state.user.data.photos){
-      return this.state.user.data.photos[0];
     }
-  }
-  getNumBikesOut(){
-    // return this.state.bikesLoaned.length;
-    return 2;
-  }
+  } 
+    componentDidMount() {
+      const id = this.props.match.params.id;
+      // fetch bike record from api using id
+      console.log('id', id)
+      
+      //get bikes that are On Loan to the User with id
+      //ID_Status = 3 = On Loan
+      API.get(`bikes`, {'filters[ID_User]': id, /*'filters[ID_Status]':3 */ }).then(data => {
+        console.log(data)
+        this.setState({bikes: data})
+        console.log(this.state.bikes.data.data);
+
+      })
+      API.get(`users/${id}`).then(userData => {
+        console.log(userData)
+        this.setState({user: userData});
+      })
+    }
+
+    getPhoto(){
+      if(this.state.user.data.photos){
+        return this.state.user.data.photos[0];
+      }
+    }
+    getNumBikesOut(){
+      return this.state.bikes.data.data.length;
+    }
+
 
   getTableRows(){
     if(this.getNumBikesOut() > 0) {
       //Write table row for each bike loaned to user/user's children
 
-      return this.state.bikesLoaned.map(function(element) {
+      return this.state.bikes.data.data.map(function(element) {
         return <tr>
-          <td>{element.ID_Bike}</td>
+          <td>{element.BikeLabel}</td>
           <td>{element.overDue}</td>
           <td>{element.dueDate}</td>
         </tr>;
@@ -107,13 +89,14 @@ export default class ShowUser extends React.Component {
   render() {
     return (
       <DefaultLayout>
+        <h2>{`${this.state.user.data.FirstName} ${this.state.user.data.LastName}`}</h2>
         <img src= {this.getPhoto()}/>
         <p>{this.state.user.FirstName} {this.state.user.LastName} </p>
-        <h3>Bikes Loaned</h3>
+        <h2>Bikes Loaned</h2>
         <Table>
           <tbody>
             <tr>
-              <th>Bike ID</th>
+              <th>Bike Label</th>
               <th>OverDue?</th>
               <th>Due Date</th>
             </tr>
@@ -139,12 +122,12 @@ export default class ShowUser extends React.Component {
               <td>{`${this.state.user.data.PostalCode}`}</td>
             </tr>
             <tr>
-              <td>Children Under 18</td>
-              <td>{`${this.state.user.data.TireSize}`}</td>
+              <td>Photo Consent</td>
+              <td>{`${this.state.user.data.PhotoConsent}`}</td>
             </tr>
           </tbody>
         </table>         
       </DefaultLayout>
     );
   }
-}
+} 
