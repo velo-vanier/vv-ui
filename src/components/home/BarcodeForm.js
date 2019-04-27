781a28c1a2700e28520d4eb27eb323e430e390b9
Tina <tinavashistha@cmail.carleton.ca>
2019-04-27 18:55:06 -0400

    Updated into full react component

--- src/components/home/BarcodeForm.js
+++ src/components/home/BarcodeForm.js
@@ -1,9 +1,20 @@
-<FormGroup>
-    <Label for="barcode"><h1>{labels.scanBarcode}</h1></Label>
-    <Input
-      type="text"
-      name="barcode"
-      id="barcode"
-      onChange={handleScan}
-    />
-</FormGroup>
+import React from 'react'
+import { Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
+import DefaultLayout from '../layouts/Default'
+import { labels } from "../../helpers/localization";
+
+const BarcodeForm = props => {
+  return(
+    <FormGroup>
+        <Label for="barcode"><h1>{labels.scanBarcode}</h1></Label>
+        <Input
+          type="text"
+          name="barcode"
+          id="barcode"
+          onChange={handleScan}
+        />
+    </FormGroup>
+  )
+}
+
+export default BarcodeForm;
