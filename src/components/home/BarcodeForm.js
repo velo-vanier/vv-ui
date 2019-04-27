3a83067ce89bbf30b0a69dc3644eadeec8ca7311
Tina <tinavashistha@cmail.carleton.ca>
2019-04-27 13:56:28 -0400

    Moved Barcode code from Home.js to BarcodeForm.js

---
+++ src/components/home/BarcodeForm.js
@@ -0,0 +1,9 @@
+<FormGroup>
+    <Label for="barcode"><h1>{labels.scanBarcode}</h1></Label>
+    <Input
+      type="text"
+      name="barcode"
+      id="barcode"
+      onChange={handleScan}
+    />
+</FormGroup>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,16 +20,7 @@ const Home = props => {
             <Container>
                 <Row className="justify-content-center">
                     <Col>
-                      /* BARCODE <FormGroup>
-                          <Label for="barcode"><h1>{labels.scanBarcode}</h1></Label>
-                          <Input
-                            type="text"
-                            name="barcode"
-                            id="barcode"
-                            onChange={handleScan}
-                          />
-                        </FormGroup>
-                        */
+                      
                     </Col>
                 </Row>
             </Container>
