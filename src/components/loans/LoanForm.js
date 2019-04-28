5bc3d4f60ddd05e22157766f2671964049ec6060
Tina <tinavashistha@cmail.carleton.ca>
2019-04-28 10:58:09 -0400

    Added submit button

--- src/components/loans/LoanForm.js
+++ src/components/loans/LoanForm.js
@@ -7,6 +7,7 @@ import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import ImageUploader from 'react-images-upload';
 import 'react-datepicker/dist/react-datepicker.css';
+import DefaultLayout from "../layouts/Default";
 
 export default class LoanForm extends React.Component {
   constructor(props){
@@ -125,173 +126,180 @@ export default class LoanForm extends React.Component {
     const { bikes } = this.state;
 
     return (
-      <div>
-        <FormGroup>
-          <Label for="user">{labels.selecUser}</Label>
-          {this.state.selectedUser === null ?
-            <LookupSelect
-              placeholder={labels.searchUserPlaceholder}
-              entity="users"
-              apiParams={{ "filters[Role]": "Borrower" }}
-              renderItem={user => user.FirstName + " " + user.LastName}
-              onSelect={user => this.selectUser(user)}
-            />
-            :
-            <Input
-              type="text"
-              value={this.state.selectedUser.FirstName + " " + this.state.selectedUser.LastName}
-              onClick={e => this.reset()}
-            />
-          }
-        </FormGroup>
+      <DefaultLayout>
+        <Container>
+          <Row>
+            <Col xs="10">
+              <FormGroup>
+                <Label for="user">{labels.selecUser}</Label>
+                {this.state.selectedUser === null ?
+                  <LookupSelect
+                    placeholder={labels.searchUserPlaceholder}
+                    entity="users"
+                    apiParams={{ "filters[Role]": "Borrower" }}
+                    renderItem={user => user.FirstName + " " + user.LastName}
+                    onSelect={user => this.selectUser(user)}
+                  />
+                  :
+                  <Input
+                    type="text"
+                    value={this.state.selectedUser.FirstName + " " + this.state.selectedUser.LastName}
+                    onClick={e => this.reset()}
+                  />
+                }
+              </FormGroup>
 
-        <h4>{this.props.borrower}</h4>
-        <FormGroup>
-          <Label htmlFor="bike">{labels.selecBike}</Label>
-          {bikes.map((bike, i) => (<div key={i}>
-            {bike.BikeLabel} <code onClick={() => this.removeBike(bike)}>X</code>
-          </div>))}
-          <LookupSelect
-            entity="bikes"
-            apiParams={{"filters[ID_Status]": "2"}}
-            placeholder={labels.searchBikePlaceholder}
-            onSelect={(bike) => this.selectBike(bike)}
-            renderItem={bike =>
-              <Row>
-                <Col md="6">
-                  <strong>{bike.BikeLabel} {bike.TireSize}</strong>
-                </Col>
-                <Col md="6">
-                  <i>{bike.Description}</i>
-                </Col>
-              </Row>
-            }
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="loadDate">{labels.loanDate}</label>
-          <DatePicker
-            className="form-control"
-            selected={this.state.loanDate}
-            onChange={date => this.changeLoanDate(date)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="returnDate">{labels.returnDate}</label>
-          <DatePicker
-            className="form-control"
-            selected={this.state.returnDate}
-            onChange={date => this.changeReturnDate(date)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Container>
-            <Row>
-              <Col xs="6">
-                <Label check>
-                  <Input
-                    type="checkbox"
-                    checked={this.state.helmet > 0}
-                    onChange={e => this.helmetChecked(e)}
-                  />{" "}
-                  {labels.helmet}
-                </Label>
-              </Col>
-              <Col xs="3">
-                {this.state.helmet > 0 &&
-                  <Input
-                    type="select"
-                    value={this.state.helmet}
-                    onChange={e => this.helmetNumberChange(e)}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Input>
-                }
-              </Col>
-            </Row>
-          </Container>
-        </FormGroup>
-        <FormGroup>
-          <Container>
-            <Row>
-              <Col xs="6">
-                <Label check>
-                  <Input
-                    type="checkbox"
-                    checked={this.state.lights > 0}
-                    onChange={e => this.lightsChecked(e)}
-                  />{" "}
-                  {labels.lights}
-                </Label>
-              </Col>
-              <Col xs="3">
-                {this.state.lights > 0 &&
-                  <Input
-                    type="select"
-                    value={this.state.lights}
-                    onChange={e => this.lightsNumberChange(e)}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Input>
-                }
-              </Col>
-            </Row>
-          </Container>
-        </FormGroup>
-        <FormGroup>
-          <Container>
-            <Label check>
-              <Input
-                type="checkbox"
-                checked={this.state.locks.length > 0}
-                onChange={e => this.locksChecked(e)}
-              />{" "}
-              {labels.locks}
-            </Label>
-            {this.state.locks.length > 0 &&
-              <div>
-                {
-                  this.state.locks.map((lock, i) => <FormGroup key={i}>
+              <h4>{this.props.borrower}</h4>
+              <FormGroup>
+                <Label htmlFor="bike">{labels.selecBike}</Label>
+                {bikes.map((bike, i) => (<div key={i}>
+                  {bike.BikeLabel} <code onClick={() => this.removeBike(bike)}>X</code>
+                </div>))}
+                <LookupSelect
+                  entity="bikes"
+                  apiParams={{"filters[ID_Status]": "2"}}
+                  placeholder={labels.searchBikePlaceholder}
+                  onSelect={(bike) => this.selectBike(bike)}
+                  renderItem={bike =>
+                    <Row>
+                      <Col md="6">
+                        <strong>{bike.BikeLabel} {bike.TireSize}</strong>
+                      </Col>
+                      <Col md="6">
+                        <i>{bike.Description}</i>
+                      </Col>
+                    </Row>
+                  }
+                />
+              </FormGroup>
+              <FormGroup>
+                <label htmlFor="loadDate">{labels.loanDate}</label>
+                <DatePicker
+                  className="form-control"
+                  selected={this.state.loanDate}
+                  onChange={date => this.changeLoanDate(date)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <label htmlFor="returnDate">{labels.returnDate}</label>
+                <DatePicker
+                  className="form-control"
+                  selected={this.state.returnDate}
+                  onChange={date => this.changeReturnDate(date)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Container>
+                  <Row>
+                    <Col xs="6">
+                      <Label check>
+                        <Input
+                          type="checkbox"
+                          checked={this.state.helmet > 0}
+                          onChange={e => this.helmetChecked(e)}
+                        />{" "}
+                        {labels.helmet}
+                      </Label>
+                    </Col>
+                    <Col xs="3">
+                      {this.state.helmet > 0 &&
+                        <Input
+                          type="select"
+                          value={this.state.helmet}
+                          onChange={e => this.helmetNumberChange(e)}>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </Input>
+                      }
+                    </Col>
+                  </Row>
+                </Container>
+              </FormGroup>
+              <FormGroup>
+                <Container>
+                  <Row>
+                    <Col xs="6">
+                      <Label check>
+                        <Input
+                          type="checkbox"
+                          checked={this.state.lights > 0}
+                          onChange={e => this.lightsChecked(e)}
+                        />{" "}
+                        {labels.lights}
+                      </Label>
+                    </Col>
+                    <Col xs="3">
+                      {this.state.lights > 0 &&
+                        <Input
+                          type="select"
+                          value={this.state.lights}
+                          onChange={e => this.lightsNumberChange(e)}>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </Input>
+                      }
+                    </Col>
+                  </Row>
+                </Container>
+              </FormGroup>
+              <FormGroup>
+                <Container>
+                  <Label check>
                     <Input
-                      type="text"
-                      value={lock}
-                      placeholder={labels.lockPlaceholder}
-                      onChange={e => this.changeLock(i, e)}
-                    />
-                  </FormGroup>)
+                      type="checkbox"
+                      checked={this.state.locks.length > 0}
+                      onChange={e => this.locksChecked(e)}
+                    />{" "}
+                    {labels.locks}
+                  </Label>
+                  {this.state.locks.length > 0 &&
+                    <div>
+                      {
+                        this.state.locks.map((lock, i) => <FormGroup key={i}>
+                          <Input
+                            type="text"
+                            value={lock}
+                            placeholder={labels.lockPlaceholder}
+                            onChange={e => this.changeLock(i, e)}
+                          />
+                        </FormGroup>)
+                      }
+                      <Button color="link"
+                        onClick={e => this.addLock(e)}
+                      >{labels.addLock}</Button>
+                    </div>
+                  }
+                </Container>
+              </FormGroup>
+              <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={p => this.onSelect(p)}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+              />
+              <FormGroup>
+                {this.state.picture &&
+                  <img
+                    style={{
+                      maxWidth: 200,
+                      maxHeight: 200
+                    }}
+                    src={URL.createObjectURL(this.state.picture)}
+                  />
                 }
-                <Button color="link"
-                  onClick={e => this.addLock(e)}
-                >{labels.addLock}</Button>
-              </div>
-            }
-          </Container>
-        </FormGroup>
-        <ImageUploader
-          withIcon={true}
-          buttonText='Choose images'
-          onChange={p => this.onSelect(p)}
-          imgExtension={['.jpg', '.gif', '.png', '.gif']}
-          maxFileSize={5242880}
-        />
-        <FormGroup>
-          {this.state.picture &&
-            <img
-              style={{
-                maxWidth: 200,
-                maxHeight: 200
-              }}
-              src={URL.createObjectURL(this.state.picture)}
-            />
-          }
-        </FormGroup>
-      </div >
+              </FormGroup>
+              <a href="confirmation" class="btn btn-lg btn-success">Submit</a>
+            </Col>
+          </Row>
+        </Container>
+      </DefaultLayout>
     );
   }
 
