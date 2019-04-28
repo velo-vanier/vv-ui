bca0b29dd037d40153dfa2914f1cdda0879ba5dc
Dragan Nevjestic <office@dragan.ca>
2019-04-28 11:56:13 -0400

    Updated Return page
    Added form the finalize returns: bike status and notes. Needs to hooked to the API.

--- src/components/loans/LoanReturn.js
+++ src/components/loans/LoanReturn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from "reactstrap";
+import { Container, Row, Col, Card, Input, FormGroup, Label, FormText } from "reactstrap";
 import DefaultLayout from "../layouts/Default";
 import { labels } from "../../helpers/localization";
 import ReturnFormAccessories  from './ReturnFormAccessories'
@@ -37,7 +37,7 @@ const LoanReturn = props => {
              <Col xs="6">
              <h3>Bike Loaned</h3>
              <p>C05</p>
-             
+
              <div><strong>Loan Date: </strong>03/10/2019</div>
              <div><strong>Return Date: </strong>04/10/2019</div>
 
@@ -49,6 +49,26 @@ const LoanReturn = props => {
                </Col>
                </Row>
           </Card>
+
+          <h3>Bike status</h3>
+          <Input type="select" bsSize="lg">
+            <option value="2">Available</option>
+            <option value="3">On loan</option>
+            <option value="4">On hold</option>
+            <option value="5">Testing</option>
+            <option value="6">In repair</option>
+            <option value="7">MIA</option>
+            <option value="8">Scrapped</option>
+            <option value="9">Give away</option>
+            </Input>
+
+            <FormGroup>
+                  <Label for="exampleText">Notes</Label>
+                  <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+
+            <a href="listing" class="btn btn-lg btn-success">Return</a> 
+
 
       </Container>
     </DefaultLayout>
